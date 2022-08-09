3ffc79c8487a48432af9a8f1ca9f204c3d9c3549
huic05 <huitungc1228@gmail.com>
2022-08-09 13:31:23 -0400

    h

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -10,22 +10,19 @@ public class SceneLoader : MonoBehaviour
     public string shop;
     public string credits;
     public string menu;
-    private Canvas _pause;
     private Canvas _scene;
     private GameObject _game;
-    private Canvas _settings;
     private Canvas _menu;
+    private GameObject _back;
     // Start is called before the first frame update
     void Start()
     {
-        _pause = GameObject.Find("Pause").GetComponent<Canvas>();
-        _pause.enabled = false;
         _scene = GameObject.Find("Canvas").GetComponent<Canvas>();
         _scene.enabled = true;
         _game = GameObject.Find("Game");
         _game.SetActive(true);
-        _settings = GameObject.Find("Settings").GetComponent<Canvas>();
         _menu = GameObject.Find("Menu").GetComponent<Canvas>();
+        
     }
 
     // Update is called once per frame
@@ -48,9 +45,9 @@ public class SceneLoader : MonoBehaviour
     }
     public void ScenePause()
     {
-        _pause.enabled = true;
         _scene.enabled = false;
         _game.SetActive(false);
+        _back.
     }
     public void Menu()
     {
@@ -59,14 +56,12 @@ public class SceneLoader : MonoBehaviour
     public void Back()
     {
         _scene.enabled = true;
-        _pause.enabled = false;
         _game.SetActive(true);
-        _settings.enabled = false;
     }
     public void Settings()
     {
         _menu.enabled = false;
-        _settings.enabled = true;
+     
     }
 
 }

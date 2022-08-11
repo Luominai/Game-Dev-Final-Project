a55c0293ba3e2cb989069677b06953aa157e35d6
Luominai <108951684+Luominai@users.noreply.github.com>
2022-08-11 13:24:20 -0400

    testing a data script

---
+++ Assets/Scripts/DataTesting.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DataTesting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private static DataTesting data;
+    public float masterVolume = 0;
+    public float SFXVolume = 0;
+    public float musicVolume = 0;
+    public bool leftHandMode = false;
+    public int coins = 0;
+    void Awake()
+    {
+        if (data != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        data = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void loadLevel1()
+    {
+        SceneManager.LoadScene("Kevin's test scene");
+    }
+}

1c715448a71ba79978415151c3225332d8f25889
huic05 <huitungc1228@gmail.com>
2022-08-11 15:44:37 -0400

    more cut scene

--- Assets/Scripts/CutSceneMenu.cs
+++ Assets/Scripts/CutSceneMenu.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CutSceneMenu : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(Transition());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private IEnumerator Transition()
+    {
+        yield return new WaitForSeconds(24);
+        SceneManager.LoadScene("MenuScene");
     }
 }
